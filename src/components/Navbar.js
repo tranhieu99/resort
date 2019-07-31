7af5d09d595237b67e1a6c065af17c99e8cd0ab7
TranHieu <a365@Hieus-MacBook.local>
2019-08-01 00:10:57 +0700

    repair link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ export default function Navbar(props) {
                         <Link to = "/">Home</Link>
                     </li>
                     <li>
-                        <Link to = "/room">Rooms</Link>
+                        <Link to = "/rooms">Rooms</Link>
                     </li>
                 </ul>
             </div>
