ed0c42d60c767806da509459bab7086fee5e17ec
Hieu <hieutt1348@gmail.com>
2019-09-12 21:54:34 +0700

    complete single room page

--- src/page/SingleRoom.js
+++ src/page/SingleRoom.js
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom'
 import {roomContext} from '../context';
 import defaultImg from '../images/room-1.jpeg'
 import StyledHero from '../components/StyledHero'
+import Title from '../components/Title'
 export default class SingleRoom extends Component {
     constructor(props){
         super(props)
@@ -24,16 +25,40 @@ export default class SingleRoom extends Component {
                 <Link to  = "/rooms" className="btn-primary"> Back To Rooms</Link>    
             </div>
         }
-        const {name, slug, type, price, size,pets,images,extras, description,breakfast} = room
+        const {name,capacity ,price, size,pets,images,extras, description,breakfast} = room;
+        let randomImage = Math.floor(Math.random() * (images.length));
         return (
-            <div>
-                <StyledHero img = {images[0] || this.state.defaultImg} >
+            <>
+                <StyledHero img = {images[randomImage] || this.state.defaultImg} >
                     <Banner title = {name} >
                         <Link to  = "/rooms" className="btn-primary"> Back To Rooms</Link>    
                     </Banner>
                 </StyledHero>
-            
-            </div>
+                <Title title = {`${name} images`} />
+                <div className="single-room-images baongoai">
+                        {images.map((image,index) => <div className="single-room-images__image"  key = {index}> <img src = {image} alt ={name} className="responsive-img"/> </div>)}
+                </div>
+                <section className = "single-room-information baongoai">
+                    <article className="single-room-information__left">
+                        <h2>Detail</h2>
+                        <p>{description}</p>
+                    </article>
+                    <article className="single-room-information__right">
+                        <h2>Info</h2>
+                        <p>Price : ${price}</p>
+                        <p>Size : ${size} SQFT</p>
+                        <p>{capacity > 1 ? `Capacity: ${capacity} people` : "Capacity: 1 person"}</p>
+                        <p> {pets ? "Pets allowed" : "No pets allowed"} </p>
+                        <p>{breakfast && "Break fast include"}</p>
+                    </article>
+                </section>
+                <section className = "single-room-extras baongoai">
+                    <h3>Extras</h3>
+                    <ul>
+                        {extras.map((item,index) => <li key = {index}>- {item} </li> )}
+                    </ul>
+                </section>
+            </>
         )
     }
 }
